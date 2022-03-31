13025249a8108849614d107ce71f0a308e64518e
isaiaslb <isaias9713@hotmail.com>
2022-03-31 13:48:11 -0300

    alteração no login

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ function EnviarForm() {
         if (login == 'responsavel') {
             localStorage.setItem('acesso', 'resp')
             localStorage.setItem('chat-tipo-login', 1)
-            
+
         } else {
-            localStorage.setItem('acesso', 'aluno')  
-            localStorage.setItem('chat-tipo-login', 2)  
+            localStorage.setItem('acesso', 'aluno')
+            localStorage.setItem('chat-tipo-login', 2)
         }
 
 
@@ -26,11 +26,41 @@ function EnviarForm() {
 }
 
 function Exit() {
-  
-        localStorage.removeItem('acesso')
-        localStorage.removeItem('chat-tipo-login')
-        window.location.reload(true)
 
-        location.href = 'index.html';
+    localStorage.removeItem('acesso')
+    localStorage.removeItem('chat-tipo-login')
+    window.location.reload(true)
 
+    location.href = 'login.html';
+
+}
+
+function Login() {
+    var done = 0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+
+    if (usuario == "responsavel") {
+        localStorage.setItem('acesso', 'resp')
+        localStorage.setItem('chat-tipo-login', 1)
+        window.location = "sistema.html";
+        done = 1;
+    } else if (usuario == "aluno") {
+        localStorage.setItem('acesso', 'aluno')
+        localStorage.setItem('chat-tipo-login', 2)
+        window.location = "sistema.html";
+        done = 1;
+    } else if (usuario == "responsavel-legal") {
+        localStorage.setItem('acesso', 'resp-legal')
+        localStorage.setItem('chat-tipo-login', 3)
+        window.location = "sistema.html";
+        done = 1;
+    } else if (usuario == "responsavel-fin") {
+        localStorage.setItem('acesso', 'resp-fin')
+        localStorage.setItem('chat-tipo-login', 4)
+        window.location = "sistema.html";
+        done = 1;
+    }
+
+    if (done == 0) { alert("Dados incorretos, tente novamente"); }
 }
